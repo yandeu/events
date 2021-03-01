b66c74a8aaa03bb8f4040457925f7d7c58ab2b76
yandeu <20306025+yandeu@users.noreply.github.com>
2021-03-01 01:47:16 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,29 @@ type EventArgs<T extends ValidEventMap, K extends EventNames<T>> = Parameters<Ev
 
 type EventNames<T extends ValidEventMap> = T extends string | symbol ? T : keyof T
 
+class EE {
+  constructor(public fn: any, public context: any, public once = false) {}
+}
+
+function addListener(emitter: Events<any>, event: any, fn: any, context: any, once: any) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('The listener must be a function')
+  }
+
+  const listener = new EE(fn, context || emitter, once)
+
+  if (!emitter._events.has(event)) emitter._events.set(event, listener), emitter._eventsCount++
+  else if (!emitter._events.get(event).fn) emitter._events.get(event).push(listener)
+  else emitter._events.set(event, [emitter._events.get(event), listener])
+
+  return emitter
+}
+
+const clearEvent = (emitter: Events<any>, event: any) => {
+  if (--emitter._eventsCount === 0) emitter._events = new Map()
+  else emitter._events.delete(event)
+}
+
 export class Events<EventMap extends ValidEventMap> {
   _events: Map<EventNames<EventMap>, any> = new Map()
   _eventsCount = 0
@@ -41,9 +64,110 @@ export class Events<EventMap extends ValidEventMap> {
   public eventNames() {
     return Array.from(this._events.keys())
   }
-  public on<T extends EventNames<EventMap>>(event: T, fn: EventListener<EventMap, T>) {}
 
-  public emit<T extends EventNames<EventMap>>(event: T, ...args: EventArgs<EventMap, T>) {}
+  public listeners(event: EventNames<EventMap>) {
+    const handlers = this._events.get(event)
+
+    if (!handlers) return []
+    if (handlers.fn) return [handlers.fn]
+
+    for (var i = 0, l = handlers.length, ee = new Array(l); i < l; i++) {
+      ee[i] = handlers[i].fn
+    }
+
+    return ee
+  }
+
+  public listenerCount(event: EventNames<EventMap>) {
+    let listeners = this._events.get(event)
+
+    if (!listeners) return 0
+    if (listeners.fn) return 1
+    return listeners.length
+  }
+
+  public emit<T extends EventNames<EventMap>>(event: T, ...args: EventArgs<EventMap, T>) {
+    if (!this._events.has(event)) return false
+
+    let listeners = this._events.get(event)
+    let i
+
+    if (listeners.fn) {
+      if (listeners.once) this.removeListener(event, listeners.fn, undefined, true)
+      return listeners.fn.call(listeners.context, ...args), true
+    } else {
+      let length = listeners.length
+
+      for (i = 0; i < length; i++) {
+        if (listeners[i].once) this.removeListener(event, listeners[i].fn, undefined, true)
+        listeners[i].fn.call(listeners[i].context, ...args)
+      }
+    }
+
+    return true
+  }
+
+  public on<T extends EventNames<EventMap>>(event: T, fn: EventListener<EventMap, T>, context?: any) {
+    return addListener(this, event, fn, context, false)
+  }
+
+  public once<T extends EventNames<EventMap>>(event: T, fn: EventListener<EventMap, T>, context?: any) {
+    return addListener(this, event, fn, context, true)
+  }
+
+  removeListener<T extends EventNames<EventMap>>(
+    event: T,
+    fn?: EventListener<EventMap, T>,
+    context?: any,
+    once?: boolean
+  ) {
+    if (!this._events.has(event)) return this
+    if (!fn) {
+      clearEvent(this, event)
+      return this
+    }
+
+    let listeners = this._events.get(event)
+
+    if (listeners.fn) {
+      if (listeners.fn === fn && (!once || listeners.once) && (!context || listeners.context === context)) {
+        clearEvent(this, event)
+      }
+    } else {
+      for (var i = 0, events = [], length = listeners.length; i < length; i++) {
+        if (listeners[i].fn !== fn || (once && !listeners[i].once) || (context && listeners[i].context !== context)) {
+          events.push(listeners[i])
+        }
+      }
+
+      // Reset the array, or remove it completely if we have no more listeners.
+      if (events.length) this._events.set(event, events.length === 1 ? events[0] : events)
+      else clearEvent(this, event)
+    }
+
+    return this
+  }
+
+  removeAllListeners(event?: EventNames<EventMap>) {
+    if (event) {
+      if (this._events.delete(event)) clearEvent(this, event)
+    } else {
+      this._events = new Map()
+      this._eventsCount = 0
+    }
+
+    return this
+  }
+
+  // alias
+  get off() {
+    return this.removeListener
+  }
+
+  // alias
+  get addListener() {
+    return this.on
+  }
 }
 
 const events = new Events<EventMap>()
@@ -52,10 +176,14 @@ events.on('something', (a, b, c) => {
   console.log(a)
 })
 
+events.once('error', s => {
+  console.log('error', s)
+})
+
 console.log(events.eventNames())
 
 events.emit('error', 'asfd')
 
 events.emit('error', 'asfdasf')
 
-events.emit('something', 2, { hello: 'sdf' }, [3, 3, 'k'])
+events.emit('something', 5874, { hello: 'sdf' }, [3, 3, 'k'])

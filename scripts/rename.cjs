0c14382906f903af4e85c97036c09b2f7675439e
yandeu <20306025+yandeu@users.noreply.github.com>
2021-03-02 15:27:59 +0100

    Improved rename script

--- scripts/rename.cjs
+++ scripts/rename.cjs
@@ -1,7 +1,48 @@
+/**
+ * @description rename .js to .mjs
+ */
+
 const fs = require('fs')
 const path = require('path')
 
-// rename .js to .mjs
-fs.rename(path.resolve(__dirname, '../esm/index.js'), path.resolve(__dirname, '../esm/index.mjs'), function (err) {
-  if (err) console.log('ERROR: ' + err)
+const dir = path.resolve(__dirname, '../esm')
+
+const replaceName = fileName => {
+  return fileName.replace(/\.js$/, '.mjs').replace(/\.js\.map$/, '.mjs.map')
+}
+
+const replaceReferences = (fileName, file) => {
+  const fileNameWithoutExtension = path.basename(fileName).split('.').shift()
+
+  const reg1 = new RegExp(`${fileNameWithoutExtension}\.js\.map`)
+  const reg2 = new RegExp(`${fileNameWithoutExtension}\.js`)
+
+  return file.replace(reg1, `${fileNameWithoutExtension}.mjs.map`).replace(reg2, `${fileNameWithoutExtension}.mjs`)
+}
+
+fs.readdir(dir, (err, files) => {
+  if (err) return console.log('ERROR: ' + err)
+
+  for (const file of files) {
+    const fileName = `${dir}/${file}`
+
+    const newFileName = replaceName(fileName)
+
+    // rename file
+    fs.rename(fileName, newFileName, err => {
+      if (err) return console.log('ERROR: ' + err)
+
+      // replace reference inside files
+      fs.readFile(newFileName, 'utf8', (err, data) => {
+        if (err) return console.log('ERROR: ' + err)
+
+        const modification = replaceReferences(fileName, data)
+
+        // overwrite file
+        fs.writeFile(newFileName, modification, 'utf8', err => {
+          if (err) return console.log('ERROR: ' + err)
+        })
+      })
+    })
+  }
 })
